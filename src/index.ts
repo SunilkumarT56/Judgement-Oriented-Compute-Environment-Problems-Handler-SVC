8b8b482c30b9bf7d5d4da8712c7b8f9e6ba7ff10
Sunilkumar <sunilcse2006@gmail.com>
2025-11-29 00:03:40 +0530

    init the service

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { pool } from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
-const dbName = await pool.query("SELECT current_database()");
-const host = await pool.query("SELECT inet_server_addr()");
+import express from "express";
+import cors from "cors";
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-console.log("DB:", dbName.rows);
-console.log("Host:", host.rows);
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
