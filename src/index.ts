8eff1df83de541c7502f54c7ae2a0c85015fc08e
Sunilkumar <sunilcse2006@gmail.com>
2025-11-28 20:18:40 +0530

    config postgresql using supabase

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log("Hello World");
-// main server to provide problems , responsible for problems section
+import { pool } from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+const dbName = await pool.query("SELECT current_database()");
+const host = await pool.query("SELECT inet_server_addr()");
+
+console.log("DB:", dbName.rows);
+console.log("Host:", host.rows);
